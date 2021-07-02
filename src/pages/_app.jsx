7f9352f3d2b261d8cf32b38866747e937cae0951
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-07-02 22:16:03 +0800

    feat: add nextjs-progressbar

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,13 @@
 import '~/styles/tailwind.css'
+import NProgress from '~/lib/NextProgressBar'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NProgress />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
